f2375230e24ac73a195748521378cca9295700a9
zorothecoder00 <awledouamoussou95@gmail.com>
2025-02-06 11:33:35 +0000

    Ajout du script pour le défilement des photos de la page d'accueil , suppression du mb des photos de la Section Modeles , changement du background de la section contact et suppression de la navbar dans le footer

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const images = document.querySelectorAll(".slide");
+    const images = document.querySelectorAll(".slide"); 
 
     const observer = new IntersectionObserver(entries => {
         entries.forEach(entry => {
@@ -29,8 +29,20 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 });
 
-
 document.addEventListener("DOMContentLoaded", function () {
+ // Tableau des images de fond
+    const imagesFond = ['fashion6.jpg', 'fashion11.webp', 'fashion12.webp']; 
+    let currentImageFondIndex = 0;
+ 
+    // Fonction pour changer l'image de fond
+    function changeBackgroundImageFond() {
+        currentImageFondIndex = (currentImageFondIndex + 1) % imagesFond.length; // Cycle à travers les images
+        const backgroundElement = document.getElementById('background-image');
+        backgroundElement.style.backgroundImage = "url(" + imagesFond[currentImageFondIndex] + ")";
+
+    }
+
+    // Fonction de zoom pour l'image de fond
     const bg = document.querySelector(".bg-zoom");
 
     function zoomIn() {
@@ -41,9 +53,12 @@ document.addEventListener("DOMContentLoaded", function () {
         bg.style.backgroundSize = "100%";
     }
 
+    // Changer l'image toutes les 5 secondes
+    setInterval(changeBackgroundImageFond, 5000); 
+
+    // Zoom toutes les 6 secondes (décalé légèrement par rapport à l'image pour éviter les conflits)
     setInterval(() => {
         zoomIn();
         setTimeout(zoomOut, 3000); // Reviens à la taille normale après 3s
-    }, 6000); // Répète toutes les 6 secondes
+    }, 6000); // Répète toutes les 6 seconde
 });
-
